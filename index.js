365881b9e8438083f37190376d8975c4b96abc4e
Raghav <rgandotra14@e.com>
2021-06-01 20:41:44 +0530

    nav stick on scroll

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hi");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 230 || document.documentElement.scrollTop > 230) {
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.position="fixed";
+  } else {
+    document.getElementById("navbar").style.position="relative";
+  }
+}
